8e8b586cf0f0b10526d99d970d703ffacf847113
Ben Pickles <spideryoung@gmail.com>
2009-11-06 15:48:40 +0000

    Bring in some tests from the old Sinatra project.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,6 @@
+config.gem 'factory_girl'
+config.gem 'shoulda'
+
 # Settings specified here will take precedence over those in config/environment.rb
 
 # The test environment is used exclusively to run your application's
---
+++ test/unit/wave_test.rb
@@ -0,0 +1,60 @@
+require 'test_helper'
+
+class WaveTest < ActiveSupport::TestCase
+  context '#interpolate' do
+    context 'with search term' do
+      setup do
+        @wave = Factory(:wave, :uri => 'http://example.com/?q=%s')
+      end
+
+      should 'empty' do
+        assert_equal 'http://example.com/?q=', @wave.interpolate(nil)
+      end
+
+      should 'simple' do
+        assert_equal 'http://example.com/?q=simple', @wave.interpolate('simple')
+      end
+
+      should 'URL encode' do
+        assert_equal 'http://example.com/?q=two%20words', @wave.interpolate('two words')
+      end
+    end
+
+    context 'without search term' do
+      setup do
+        @wave = Factory(:wave, :uri => 'http://example.com')
+      end
+
+      should 'simple' do
+        assert_equal 'http://example.com', @wave.interpolate('blah')
+      end
+    end
+
+    context 'numbered placeholders' do
+      should 'just numbers' do
+        @wave = Factory(:wave, :uri => 'http://example.com/?a=%1&b=%2')
+        assert_equal 'http://example.com/?a=a&b=b', @wave.interpolate('a b')
+      end
+
+      should 'numbers and string' do
+        @wave = Factory(:wave, :uri => 'http://example.com/?q=%s&a=%1&b=%2')
+        assert_equal 'http://example.com/?q=a%20b&a=a&b=b', @wave.interpolate('a b')
+      end
+
+      should 'numbers with remainder' do
+        @wave = Factory(:wave, :uri => 'http://example.com/?a=%1&b=%2&z=%n')
+        assert_equal 'http://example.com/?a=a&b=b&z=c%20d-z', @wave.interpolate('a b c d-z')
+      end
+
+      should 'more numbers than words' do
+        @wave = Factory(:wave, :uri => 'http://example.com/?a=%1&b=%2&c=%3')
+        assert_equal 'http://example.com/?a=a&b=&c=', @wave.interpolate('a')
+      end
+
+      should 'repeated numbers' do
+        @wave = Factory(:wave, :uri => 'http://example.com/?a=%1&b=%1&c=%n')
+        assert_equal 'http://example.com/?a=a&b=a&c=c', @wave.interpolate('a c')
+      end
+    end
+  end
+end
