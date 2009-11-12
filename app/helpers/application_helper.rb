a536559457dcc930d43077c20d53e6a4ae3bf123
Ben Pickles <spideryoung@gmail.com>
2009-11-12 18:19:13 +0000

    Add Gravatars.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 module ApplicationHelper
   def bookmarklet(user, text = "sinewave/#{user.username}")
     href = 'javascript:'
@@ -32,5 +34,15 @@ try {
 var pageTracker = _gat._getTracker("UA-117680-12");
 pageTracker._trackPageview();
 } catch(err) {}</script>|
+  end
+
+  def gravatar_tag(user)
+    return if user.email.blank?
+    image_tag(gravatar_url(user.email), :alt => '', :class => 'gravatar')
+  end
+
+  def gravatar_url(email)
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "http://www.gravatar.com/avatar/#{hash}?s=24"
   end
 end
