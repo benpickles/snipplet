094cb686f8eae99588c2c46bd3af6e55ad604e77
Ben Pickles <spideryoung@gmail.com>
2009-11-12 00:46:33 +0000

    Optimisations for minified bookmarklet.

--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -1,12 +1,14 @@
 function sinewave(username) {
-  var prompt = window.prompt('sinewaveapp.com/' + username + ':');
+  var w = window;
+  var e = encodeURIComponent;
+  var prompt = w.prompt('sinewaveapp.com/' + username + ':');
   var query = prompt.split(/\s+/);
   var command = query.shift();
-  var location = window.location.href;
+  var location = w.location;
   var parameters = [
-    'l=' + encodeURIComponent(location),
-    'q=' + encodeURIComponent(query.join(' '))
+    'l=' + e(location.href),
+    'q=' + e(query.join(' '))
   ]
   var url = 'http://sinewaveapp.com/' + username + '/' + command + '?' + parameters.join('&');
-  window.location.href = url;
+  location.href = url;
 }
