37ee69af94b8467184cbefc7fe571f448e1f10b7
Ben Pickles <spideryoung@gmail.com>
2009-11-12 22:54:56 +0000

    Refuse certain usernames.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  RESERVED_USERNAMES = %w(
+    about account admin favicon help images javascripts login logout robots
+    stylesheets user_sessions waves
+  )
+
   acts_as_authentic do |c|
     c.validates_format_of_login_field_options :with => /\A[a-z0-9_-]+\z/i,
       :message => 'should include only letters, numbers, "-" or "_"'
@@ -6,5 +11,14 @@ class User < ActiveRecord::Base
 
   has_many :waves, :dependent => :destroy
 
+  validate :reject_reserved_usernames
+
   attr_accessible :email, :name, :password, :password_confirmation, :username
+
+  private
+    def reject_reserved_usernames
+      if RESERVED_USERNAMES.include?(username)
+        errors.add(:username, 'has already been taken')
+      end
+    end
 end
---
+++ test/unit/user_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  should_have_many :waves
+
+  should_not_allow_values_for :username, 'account', 'login', 'waves' # etc
+
+  should_not_allow_mass_assignment_of :created_at, :crypted_password, :id,
+    :password_salt, :persistance_token, :updated_at
+end
