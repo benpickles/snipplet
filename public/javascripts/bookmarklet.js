7c51fb99f4b2694104b35bf4d54f94c6eb17f668
Ben Pickles <spideryoung@gmail.com>
2009-11-12 00:26:13 +0000

    Also send current location in bookmarklet.

--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -1,7 +1,12 @@
 function sinewave(username) {
-  var p = window.prompt('sinewaveapp.com/' + username + ':');
-  var q = p.split(/\s+/);
-  var c = q.shift();
-  var u = 'http://sinewaveapp.com/' + username + '/' + c + '?q=' + q.join(' ');
-  window.location.href = u;
+  var prompt = window.prompt('sinewaveapp.com/' + username + ':');
+  var query = prompt.split(/\s+/);
+  var command = query.shift();
+  var location = window.location.href;
+  var parameters = [
+    'l=' + encodeURIComponent(location),
+    'q=' + encodeURIComponent(query.join(' '))
+  ]
+  var url = 'http://sinewaveapp.com/' + username + '/' + command + '?' + parameters.join('&');
+  window.location.href = url;
 }
